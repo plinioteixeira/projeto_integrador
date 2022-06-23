e5ccab08caa1dcab17d744a371a5176874beb6e4
Leorsousa <leorsousa05@gmail.com>
2022-06-23 14:37:57 -0300

    .

--- uc5_prototypes/cabecalho.php
+++ uc5_prototypes/cabecalho.php
@@ -7,29 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="css/cabecalho.css">
     <link rel="stylesheet" href="css/rodape.css">
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="./slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"/>
 </head>
 <body>
-    <header class="d-flex justify-content-center">
-        <div id="header-container" class="d-flex align-items-center justify-content-between p-1 px-2">
-            <div class="menu-burger">
-                <div class="lines"></div>
-                <div class="lines"></div>
-                <div class="lines"></div>
-            </div>  
-            <a href="index.php" class="display-5 logo">OneUp</a>
-            <div><i class="fas fa-search fa-sm fa-fw"></i></div>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="cursos.php">Cursos</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="perfil.php">Perfil</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Log In</a>
+            </li>
+
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
         </div>
-        <nav class="navbar-menu">
-            <ul id="nav-menu">
-                <li><a href="#">Perfil</a></li>
-                <li><a href="#">Cursos</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Fale Conosco</a></li>
-            </ul>
-        </nav>
-    </header>
+    </div>
+    </nav>
--- uc5_prototypes/index.php
+++ uc5_prototypes/index.php
@@ -1,6 +1,6 @@
 <?php
-require "cabecalho.php";
 require "../inc/funcoes_cursos.php";
+require "cabecalho.php";
 $cursos = lerTodosOsCursos($conexao);
 ?>
 
